7eaf34558d5c94cee4e2cae489d745cfc19888fa
devmartin-9000 <dev.martinjack9000@gmail.com>
2025-12-20 04:51:24 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button>Button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
